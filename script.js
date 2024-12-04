1f5b2b927638a2b8e78a71d4a105f7142a7c7359
Dnyaneshwar Yadav Wagh <85550894+dnyaneshwarwagh0072@users.noreply.github.com>
2024-12-04 19:19:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-const enterButton = document.getElementById("enterBtn");
-const statusContainer = document.getElementById("status-container");
-enterButton.addEventListener("click", () => {
-    statusContainer.innerHTML = '<h1 id="status">Entered Metaverse</h1>';
-});
+ const enterButton = document.getElementById("enterBtn");
+        const statusParagraph = document.getElementById("status");
+
+        // Add an event listener to the button
+        enterButton.addEventListener("click", () => {
+            // Update the paragraph to an <h1> element with the desired text
+            statusParagraph.outerHTML = '<h1 id="status">Entered Metaverse</h1>';
+        });
